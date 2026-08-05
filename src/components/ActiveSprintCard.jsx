7845a326bfee6e2ee33822e1ccf3927563e855fa
João Gustavo <jgcmendonca@gmail.com>
2026-08-05 20:27:10 -0300

    feat(dashboard): complete ActiveSprintCard component

--- src/components/ActiveSprintCard.jsx
+++ src/components/ActiveSprintCard.jsx
@@ -1,13 +1,50 @@
 import Card from './Card'
 import CardTitle from './CardTitle'
+import StatusBadge from './StatusBadge'
+import ProgressBar from './ProgressBar'
 
-const ActiveSprintCard = ({ title }) => {
+const ActiveSprintCard = ({
+  title,
+  status,
+  badgeTitle,
+  endDays,
+  progress,
+  progressBarColor,
+  progressBarSize,
+  totalTasks,
+  completedTasks,
+}) => {
   return (
     <Card>
       <div className="flex justify-between">
         <div>
           <CardTitle title={title} className="text-main text-lg" />
+          <StatusBadge status={status} title={badgeTitle} />
         </div>
+        <div className="flex flex-col items-end">
+          <p className="text-secondary text-base">Ending in</p>
+          <p className="text-main text-base">
+            {endDays === 0 ? 'Last day' : `${endDays} days left`}
+          </p>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex justify-between">
+          <p className="text-secondary text-[11px]">Progress</p>
+          <p className="text-main text-[11px]">{progress}%</p>
+        </div>
+        <ProgressBar
+          progress={progress}
+          color={progressBarColor}
+          size={progressBarSize}
+        />
+      </div>
+
+      <div className="flex justify-end">
+        <p className="text-secondary text-base">
+          {completedTasks}/{totalTasks} tasks
+        </p>
       </div>
     </Card>
   )
