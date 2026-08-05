3110bdbfb64d7b08a6cd4de1e2076528cc3550d7
João Gustavo <jgcmendonca@gmail.com>
2026-08-05 17:56:27 -0300

    feat(dashboard): add StatusBadge component

---
+++ src/components/StatusBadge.jsx
@@ -0,0 +1,20 @@
+import { tv } from 'tailwind-variants'
+
+const badge = tv({
+  base: 'px-2 py-1 text-[11px] text-center border',
+  variants: {
+    status: {
+      active:
+        'bg-in-progress-status/10 text-in-progress-status border-in-progress-status/20',
+      done: 'bg-done-status/10 text-done-status border-done-status/20',
+      neutral:
+        'bg-pending-status/10 text-pending-status border-pending-status/20',
+    },
+  },
+})
+
+const StatusBadge = ({ status, title }) => {
+  return <div className={badge({ status })}>{title}</div>
+}
+
+export default StatusBadge
