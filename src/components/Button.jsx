755644601759058607f30452c98ac9c0b640dec4
João Gustavo <jgcmendonca@gmail.com>
2026-08-08 21:57:05 -0300

    feat: add reusable Button component and integrate into Sprints page

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+import { tv } from 'tailwind-variants'
+
+const button = tv({
+  base: 'flex items-center justify-center font-semibold rounded-sm transition hover:opacity-75 hover:cursor-pointer gap-2',
+  variants: {
+    size: {
+      md: 'px-4 py-2 text-xs ',
+      lg: 'px-3 py-6 text-base',
+    },
+    color: {
+      primary: 'text-main bg-primary-status',
+      secondary: 'text-secondary bg-transparent border border-border',
+      ghost: 'bg-transparent text-secondary',
+      danger: 'bg-high-priority text-smooth-primary',
+    },
+    disabled: {
+      true: 'cursor-not-allowed opacity-50 hover:opacity-50',
+    },
+  },
+  defaultVariants: {
+    color: 'primary',
+    size: 'md',
+  },
+})
+const Button = ({ children, color = 'primary', size = 'md', ...rest }) => {
+  return (
+    <button
+      className={button({ color, size, disabled: rest.disabled })}
+      {...rest}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
--- src/pages/Sprints.jsx
+++ src/pages/Sprints.jsx
@@ -1,5 +1,19 @@
+import Header from '../components/Header'
+import Button from '../components/Button'
+import PlusIcon from '../assets/icons/plus.svg?react'
+
 const Sprints = () => {
-  return <div className=""></div>
+  return (
+    <div className="flex flex-col gap-6 p-10">
+      <div className="flex items-center justify-between">
+        <Header title="Sprints" description="Manage your team's work cycles." />
+        <Button>
+          <PlusIcon />
+          Nova Sprint
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default Sprints
