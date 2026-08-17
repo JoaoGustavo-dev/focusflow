08250f4dad0e6df9c7faf4527bd7e6619337c7bd
João Gustavo <jgcmendonca@gmail.com>
2026-08-17 11:05:45 -0300

    feat: add PriorityBadge component

---
+++ src/components/PriorityBadge.jsx
@@ -0,0 +1,19 @@
+import { tv } from 'tailwind-variants'
+
+const priorityBadge = tv({
+  base: 'px-2 py-0.5 rounded-sm text-[11px] border text-center',
+  variants: {
+    priority: {
+      high: 'bg-high-priority/20 border-high-priority text-high-priority',
+      medium:
+        'bg-medium-priority/20 border-medium-priority text-medium-priority',
+      low: 'bg-low-priority/20 border-low-priority text-low-priority',
+    },
+  },
+})
+
+const PriorityBadge = ({ priority, title }) => {
+  return <div className={priorityBadge({ priority })}>{title}</div>
+}
+
+export default PriorityBadge
