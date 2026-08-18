eda96c7d7dc5deb54fe327885931b9c698d52eac
João Gustavo <jgcmendonca@gmail.com>
2026-08-18 16:07:52 -0300

    feat: add task deletion confirmation modal

---
+++ src/components/DeleteModal.jsx
@@ -0,0 +1,57 @@
+import { useRef } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import './DeleteModal.css'
+import { createPortal } from 'react-dom'
+import DangerIcon from '../assets/icons/danger.svg?react'
+import Button from './Button'
+
+const DeleteModal = ({
+  title,
+  description,
+  isOpen,
+  onConfirm,
+  onClose,
+  isLoading,
+}) => {
+  const nodeRef = useRef()
+
+  return createPortal(
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={isOpen}
+      timeout={500}
+      classNames="delete-modal"
+      unmountOnExit
+    >
+      <div
+        ref={nodeRef}
+        className="fixed top-0 bottom-0 left-0 flex h-screen w-screen items-center justify-center backdrop-blur"
+      >
+        {/* dialog */}
+        <div className="border-border flex flex-col border">
+          <div className="bg-high-surface border-b-border flex flex-col gap-1 p-4">
+            <div className="flex gap-2">
+              <p className="text-high-priority">
+                <DangerIcon />
+              </p>
+              <p className="text-main text-base">{title}</p>
+            </div>
+            <p className="text-secondary text-[12px]">{description}</p>
+          </div>
+
+          <div className="flex items-end gap-2 p-3">
+            <Button color="ghost" onClick={onClose}>
+              Cancelar
+            </Button>
+            <Button color="danger" onClick={onConfirm} disabled={isLoading}>
+              Confirmar
+            </Button>
+          </div>
+        </div>
+      </div>
+    </CSSTransition>,
+    document.body
+  )
+}
+
+export default DeleteModal
--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -1,10 +1,15 @@
 import PriorityBadge from './PriorityBadge'
 import StatusBadge from './StatusBadge'
 import TaskCheckbox from './TaskCheckbox'
+import Button from './Button'
 import TaskTime from './TaskTime'
 import { useUpdateTask } from '../hooks/data/use-update-task'
+import { useDeleteTask } from '../hooks/data/use-delete-task'
 import { nextTaskStatus } from '../utils/nextTaskStatus'
 import { toast } from 'sonner'
+import TrashIcon from '../assets/icons/trash.svg?react'
+import { useState } from 'react'
+import DeleteModal from './DeleteModal'
 
 const TaskRow = ({
   title,
@@ -17,7 +22,14 @@ const TaskRow = ({
   statusBadgeTitle,
   time,
 }) => {
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false)
+
   const { mutate } = useUpdateTask(sprintId, taskId)
+
+  const { mutate: deleteTask, isPending: deleteTaskLoading } = useDeleteTask(
+    sprintId,
+    taskId
+  )
 
   const taskNewStatus = (status) => {
     return nextTaskStatus[status]
@@ -29,31 +41,62 @@ const TaskRow = ({
         status: taskNewStatus(taskStatus),
       },
       {
-        onSuccess: () => toast.success('Task status sucessfully updated'),
+        onSuccess: () => toast.success('Task status successfully updated'),
         onError: () => toast.error('Error on updating task'),
       }
     )
   }
 
-  return (
-    <div className="border-border flex gap-4 border-b p-4">
-      <TaskCheckbox
-        id={taskId}
-        status={status}
-        onToggle={() => handleCheckBoxClick(status)}
-      />
-      <div className="flex flex-col">
-        <div className="flex flex-col">
-          <p className="text-main text-base">{title}</p>
-          <p className="text-secondary text-[12px]">{description}</p>
-        </div>
+  const handleDeleteClick = () => {
+    return setIsDeleteModalOpen(true)
+  }
+  const handleCloseModalClick = () => {
+    return setIsDeleteModalOpen(false)
+  }
 
-        <div className="flex gap-2 pt-1">
-          <PriorityBadge priority={priority} title={priorityBadgeTitle} />
-          <StatusBadge status={status} title={statusBadgeTitle} />
-          <TaskTime time={time} />
+  const handleDeleteTask = () => {
+    deleteTask(undefined, {
+      onSuccess: () => {
+        toast.success('Task successfully deleted')
+        setIsDeleteModalOpen(false)
+      },
+      onError: () => toast.error('Error on deleting task'),
+    })
+  }
+
+  return (
+    <div className="border-border flex items-start justify-between border-b p-4">
+      <div className="flex gap-4">
+        <TaskCheckbox
+          id={taskId}
+          status={status}
+          onToggle={() => handleCheckBoxClick(status)}
+        />
+        <div className="flex flex-col">
+          <div className="flex flex-col">
+            <p className="text-main text-base">{title}</p>
+            <p className="text-secondary text-[12px]">{description}</p>
+          </div>
+
+          <div className="flex gap-2 pt-1">
+            <PriorityBadge priority={priority} title={priorityBadgeTitle} />
+            <StatusBadge status={status} title={statusBadgeTitle} />
+            <TaskTime time={time} />
+          </div>
         </div>
       </div>
+
+      <Button color="danger" onClick={handleDeleteClick}>
+        <TrashIcon />
+      </Button>
+      <DeleteModal
+        title="Delete Task?"
+        description="This action cannot be undone"
+        isOpen={isDeleteModalOpen}
+        onConfirm={handleDeleteTask}
+        onClose={handleCloseModalClick}
+        isLoading={deleteTaskLoading}
+      />
     </div>
   )
 }
