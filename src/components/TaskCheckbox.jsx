90c6d599be1f93923f8308bbded94ece0615cb51
João Gustavo <jgcmendonca@gmail.com>
2026-08-19 17:12:01 -0300

    fix: correct icon sizing and color inheritance in TaskCheckbox

--- src/components/TaskCheckbox.jsx
+++ src/components/TaskCheckbox.jsx
@@ -1,12 +1,12 @@
 import { checkboxVariants } from '../utils/checkboxVariants'
 import LoaderIcon from '../assets/icons/loader.svg?react'
-import DoneIcon from '../assets/icons/done.svg?react'
+import CheckIcon from '../assets/icons/check.svg?react'
 
 const TaskCheckbox = ({ id, status, onToggle }) => {
   return (
     <label
       htmlFor={id}
-      className={`border-border text-main relative flex h-5 w-5 items-center rounded-xs border ${checkboxVariants[status]}`}
+      className={`border-border text-main relative flex h-6 w-6 items-center rounded-xs border ${checkboxVariants[status]}`}
     >
       <input
         type="checkbox"
@@ -15,8 +15,10 @@ const TaskCheckbox = ({ id, status, onToggle }) => {
         className="absolute inset-0 cursor-pointer opacity-0"
         onChange={onToggle}
       />
-      {status === 'in-progress' && <LoaderIcon className="animate-spin" />}
-      {status === 'done' && <DoneIcon />}
+      {status === 'in-progress' && (
+        <LoaderIcon className="h-full w-full animate-spin" />
+      )}
+      {status === 'done' && <CheckIcon className="h-full w-full" />}
     </label>
   )
 }
