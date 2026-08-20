9157c3153d43a0dac6619fb93e94f2434459e125
João Gustavo <jgcmendonca@gmail.com>
2026-08-20 12:09:29 -0300

    feat(sprint-backlog): add empty state when sprint has no tasks

--- src/components/SprintBacklog.jsx
+++ src/components/SprintBacklog.jsx
@@ -2,41 +2,62 @@ import { useGetTasks } from '../hooks/data/use-get-tasks'
 import Button from './Button'
 import PlusIcon from '../assets/icons/plus.svg?react'
 import TaskRow from './TaskRow'
+import EmptyState from './EmptyState'
 import { statusLabels } from '../utils/statusLabels'
 import { statusVariants } from '../utils/statusVariants'
 import { priorityLabels } from '../utils/priorityLabels'
+import { Fragment } from 'react'
+import NoTasks from '../assets/icons/no-tasks.svg?react'
 
 const SprintBacklog = ({ sprintId }) => {
   const { data: tasks } = useGetTasks(sprintId)
 
   return (
-    <div className="flex flex-col">
-      <div className="bg-high-surface flex justify-between p-4">
-        <p className="text-main font-display text-2xl">Sprint Backlog</p>
-        <Button>
-          <PlusIcon /> New Task
-        </Button>
-      </div>
+    <Fragment>
+      {tasks?.length === 0 ? (
+        <EmptyState
+          icon={<NoTasks />}
+          title="No tasks created yet"
+          subtitle="Start planning your sprint by adding tasks. Set
+          priorities, estimate points, and distribute the work to the
+          team."
+          buttonText={
+            <Fragment>
+              {' '}
+              <PlusIcon /> Crate your first task{' '}
+            </Fragment>
+          }
+        />
+      ) : (
+        <div className="flex flex-col">
+          <div className="bg-high-surface flex justify-between p-4">
+            <p className="text-main font-display text-2xl">Sprint Backlog</p>
+            <Button>
+              <PlusIcon /> New Task
+            </Button>
+          </div>
 
-      {tasks?.map((task) => {
-        return (
-          <TaskRow
-            key={task?.id}
-            task={task}
-            title={task?.title}
-            description={task?.description}
-            status={statusVariants[task?.status]}
-            checkBoxStatus={task?.status}
-            sprintId={Number(task?.sprintId)}
-            taskId={task?.id}
-            priority={task?.priority}
-            priorityBadgeTitle={priorityLabels[task?.priority]}
-            statusBadgeTitle={statusLabels[task?.status]}
-            time={task?.time}
-          />
-        )
-      })}
-    </div>
+          {tasks?.map((task) => {
+            return (
+              <TaskRow
+                key={task?.id}
+                task={task}
+                title={task?.title}
+                description={task?.description}
+                status={statusVariants[task?.status]}
+                checkBoxStatus={task?.status}
+                sprintId={Number(task?.sprintId)}
+                taskId={task?.id}
+                priority={task?.priority}
+                priorityBadgeTitle={priorityLabels[task?.priority]}
+                statusBadgeTitle={statusLabels[task?.status]}
+                time={task?.time}
+              />
+            )
+          })}
+        </div>
+      )}
+    </Fragment>
   )
 }
 
